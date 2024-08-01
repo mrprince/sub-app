bba9e673acf94ee8cfc0cac895c7824150ccca04
chengq4 <chengq4@lenovo.com>
2024-08-01 14:12:32 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {AgGridReact} from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
@@ -38,14 +36,6 @@ function App() {
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
             <h1>This is sub app</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
